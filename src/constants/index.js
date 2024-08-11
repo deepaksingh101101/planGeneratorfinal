1846eba669dea471c160fa6d953a56b171963358
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-08-11 23:47:03 +0530

    added margin , imported ai from asset, change text, added pulse animation

--- src/constants/index.js
+++ src/constants/index.js
@@ -78,9 +78,9 @@ export const notificationImages = [notification4, notification3, notification2];
 export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo];
 
 export const brainwaveServices = [
-  "Photo generating",
-  "Photo enhance",
-  "Seamless Integration",
+  "Comprehensive Insights",
+  "Rapid Analysis",
+  "Time Efficiency",
 ];
 
 export const brainwaveServicesIcons = [
