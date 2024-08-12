7b8ebab9cf15081d57286476b52bcba93f545923
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-08-13 01:12:28 +0530

    updated

---
+++ src/pages/form/FormPage.jsx
@@ -0,0 +1,9 @@
+import Steeper from "../../components/Steeper"
+export default function FormPage() {
+  return (
+    <div className="flex">
+    <div className="w-1/2">gg</div>
+    <div className="w-full"><Steeper/></div>
+    </div>
+  )
+}
